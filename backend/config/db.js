169454a53003126d55a96e49a0dbc7068a580093
modsuhail25 <90952903+modsuhail25@users.noreply.github.com>
2025-01-02 17:56:05 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDb = async () => {
   const conn = await mongoose.connect(
-    "mongodb+srv://modsuhail25:12345@cluster0.ldjcg.mongodb.net/"
+    ""
   );
 
   console.log(`MongoDb Connected : ${conn.connection.host}`);
